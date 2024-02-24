44a82f48d13920e49b68003e9dd8804495ec684d
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-02-24 15:11:53 +0100

    moving testers

---
+++ tester/stress_test.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+port=3000
+
+if [ "$#" -eq 1 ]; then
+	port=$1
+fi
+
+for i in {1..100}
+do
+	printf "Request $i\n"
+	curl -iL -X GET http://localhost:$port/static/type2/obj.json
+	printf "\n\n"
+done
+
+for i in {1..100}
+do
+	printf "Request $i\n"
+	curl -iL -X GET http://localhost:$port/stylesheet/style.css
+	printf "\n\n"
+done
+
+for i in {1..50}
+do
+	printf "Request $i\n"
+	curl -iL -X GET http://localhost:$port/static/type2/file.txt
+	printf "\n\n"
+done
+
+for i in {1..10}
+do
+	printf "Request $i\n"
+	curl -iL -X GET http://localhost:$port/static/type3/server.log
+	printf "\n\n"
+done
+
+for i in {1..5}
+do
+	printf "Request $i\n"
+	curl -iL -X GET http://localhost:$port/static/type2/48faec0cb53d.png
+	printf "\n\n"
+done
+
+for i in {1..10}
+do
+	printf "Request $i\n"
+	curl -iL -X GET http://localhost:$port/static/type3/unmf.js
+	printf "\n\n"
+done
