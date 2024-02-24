224f5c9833a32dc22b18f683bd796a021adb2942
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-02-24 15:15:53 +0100

    tests

--- tester/stress_test.bash
+++ tester/stress_test.bash
@@ -6,44 +6,44 @@ if [ "$#" -eq 1 ]; then
 	port=$1
 fi
 
-for i in {1..100}
+for i in {1..1000}
 do
 	printf "Request $i\n"
 	curl -iL -X GET http://localhost:$port/static/type2/obj.json
 	printf "\n\n"
 done
 
-for i in {1..100}
-do
-	printf "Request $i\n"
-	curl -iL -X GET http://localhost:$port/stylesheet/style.css
-	printf "\n\n"
-done
+# for i in {1..100}
+# do
+# 	printf "Request $i\n"
+# 	curl -iL -X GET http://localhost:$port/stylesheet/style.css
+# 	printf "\n\n"
+# done
 
-for i in {1..50}
-do
-	printf "Request $i\n"
-	curl -iL -X GET http://localhost:$port/static/type2/file.txt
-	printf "\n\n"
-done
+# for i in {1..50}
+# do
+# 	printf "Request $i\n"
+# 	curl -iL -X GET http://localhost:$port/static/type2/file.txt
+# 	printf "\n\n"
+# done
 
-for i in {1..10}
-do
-	printf "Request $i\n"
-	curl -iL -X GET http://localhost:$port/static/type3/server.log
-	printf "\n\n"
-done
+# for i in {1..10}
+# do
+# 	printf "Request $i\n"
+# 	curl -iL -X GET http://localhost:$port/static/type3/server.log
+# 	printf "\n\n"
+# done
 
-for i in {1..5}
-do
-	printf "Request $i\n"
-	curl -iL -X GET http://localhost:$port/static/type2/48faec0cb53d.png
-	printf "\n\n"
-done
+# for i in {1..5}
+# do
+# 	printf "Request $i\n"
+# 	curl -iL -X GET http://localhost:$port/static/type2/48faec0cb53d.png
+# 	printf "\n\n"
+# done
 
-for i in {1..10}
-do
-	printf "Request $i\n"
-	curl -iL -X GET http://localhost:$port/static/type3/unmf.js
-	printf "\n\n"
-done
+# for i in {1..10}
+# do
+# 	printf "Request $i\n"
+# 	curl -iL -X GET http://localhost:$port/static/type3/unmf.js
+# 	printf "\n\n"
+# done
