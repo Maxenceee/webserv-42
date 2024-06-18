7d3b7850d73786f0b40530cfcf76239bf74a7f8a
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2024-06-18 23:44:33 +0200

    fix invalid proxy forwarding when request has body + daemon

--- sources/logger/Logger.cpp
+++ sources/logger/Logger.cpp
@@ -6,13 +6,30 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 20:48:56 by mgama             #+#    #+#             */
-/*   Updated: 2024/04/15 01:32:48 by mgama            ###   ########.fr       */
+/*   Updated: 2024/06/18 23:40:40 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Logger.hpp"
 
 bool Logger::_debug = false;
+
+void	Logger::init(void)
+{
+	struct tm	*tm;
+    time_t rawtime;
+    char buf[32];
+
+    time(&rawtime);
+    tm = localtime (&rawtime);
+    int ret = strftime(buf, 32, "%T", tm);
+    buf[ret] = '\0';
+	std::stringstream ss;
+	ss << CYAN << "[" << buf << "] " << RESET;
+	ss  << WBS_PREFIX << "Starting server: New logger session" << RESET;
+	std::cerr << ss.str() << std::endl;
+	std::cout << ss.str() << std::endl;
+}
 
 void	Logger::print(const std::string &msg, const std::string &color)
 {
@@ -26,7 +43,7 @@ void	Logger::print(const std::string &msg, const std::string &color)
 	buf[ret] = '\0';
 	std::cout << CYAN << "[" << buf << "] " << RESET;
 	std::cout << color << msg << RESET;
-	std::cout << "\n";
+	std::cout << std::endl;
 }
 
 void	Logger::info(const std::string &msg)
@@ -41,7 +58,7 @@ void	Logger::info(const std::string &msg)
     buf[ret] = '\0';
 	std::cout << CYAN << "[" << buf << "] " << RESET;
 	std::cout << YELLOW << msg << RESET;
-	std::cout << "\n";
+	std::cout << std::endl;
 }
 
 void	Logger::warning(const std::string &msg, const std::string &color)
@@ -56,7 +73,7 @@ void	Logger::warning(const std::string &msg, const std::string &color)
     buf[ret] = '\0';
 	std::cerr << CYAN << "[" << buf << "] " << RESET;
 	std::cerr << color << WBS_PREFIX << msg << RESET;
-	std::cerr << "\n";
+	std::cerr << std::endl;
 }
 
 void	Logger::error(const std::string &msg, const std::string &color)
@@ -71,7 +88,7 @@ void	Logger::error(const std::string &msg, const std::string &color)
     buf[ret] = '\0';
 	std::cerr << CYAN << "[" << buf << "] " << RESET;
 	std::cerr << color << WBS_PREFIX << msg << RESET;
-	std::cerr << "\n";
+	std::cerr << std::endl;
 }
 
 void	Logger::debug(const std::string &msg, const std::string &color)
@@ -89,5 +106,5 @@ void	Logger::debug(const std::string &msg, const std::string &color)
     buf[ret] = '\0';
 	std::cout << CYAN << "[" << buf << "] " << RESET;
 	std::cout << color << msg << RESET;
-	std::cout << "\n";
+	std::cout << std::endl;
 }
